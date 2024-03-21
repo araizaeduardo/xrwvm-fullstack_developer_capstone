a90bc5726425c23233f031072117df0eda416afe
Eduardo <tech@eduardoaraiza.com>
2024-03-20 18:10:03 -0700

    Update Actions

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -12,6 +12,7 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 # def get_request(endpoint, **kwargs):
 def get_request(endpoint, **kwargs):
     params = ""

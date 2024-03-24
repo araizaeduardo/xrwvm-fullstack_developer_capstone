057966580671c23b522295827ca40786a9377fd4
Eduardo Araiza <tech@eduardoaraiza.com>
2024-03-24 01:38:07 -0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+git config --global user.email "tech@eduardoaraiza.com"
+git config --global user.name "Eduardo"
+cd server
+python3 -m pip install --upgrade pip
+python3 -m pip install virtualenv
+virtualenv djangoenv
+source djangoenv/bin/activate
+python3 -m pip install -U -r requirements.txt
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py createsuperuser
+cd frontend
+npm install
+npm run build
